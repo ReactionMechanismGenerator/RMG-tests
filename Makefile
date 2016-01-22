5092c9d3aea415824665566b19b86749996d4df4
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-22 10:51:33 -0500

    move check.sh from .travis.yml to Makefile
    use command-line argument as target name, rather than hard coding it.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 eg1:
-	mkdir -p testing/eg1
-	rm -rf testing/eg1/*
-	cp examples/rmg/eg1/input.py testing/eg1/input.py
-	@ echo "Running eg1 example"
-	python $(RMG)/rmg.py testing/eg1/input.py
+	mkdir -p testing/$@
+	rm -rf testing/$@/*
+	cp examples/rmg/$@/input.py testing/$@/input.py
+	@ echo "Running $@ example"
+	python $(RMG)/rmg.py testing/$@/input.py
+	bash check.sh $@
--- check.sh
+++ check.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+target=$1
+
 echo 'Travis Build Dir: '$TRAVIS_BUILD_DIR
 
-# Types of models to save
-eg1=minimal
-# eg2, etc...
-
-SOURCE_FOLDER=$TRAVIS_BUILD_DIR/testing/$eg1/chemkin/
+SOURCE_FOLDER=$TRAVIS_BUILD_DIR/testing/$target/chemkin/
 
 echo 'Source folder: '$SOURCE_FOLDER
 
 # check generated models:
-python $TRAVIS_BUILD_DIR/checkModels.py $SOURCE_FOLDER/chem_annotated.inp $SOURCE_FOLDER/species_dictionary.txt
+python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_annotated.inp $SOURCE_FOLDER/species_dictionary.txt
