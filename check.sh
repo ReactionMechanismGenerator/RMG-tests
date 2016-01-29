ad159b2a1231018277df41dac9a0249e4f53c4bc
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-29 13:20:11 -0500

    grep log for memory, execution time

--- check.sh
+++ check.sh
@@ -6,19 +6,32 @@ if [ -z ${TRAVIS_BUILD_DIR+x} ]; then TRAVIS_BUILD_DIR=$PWD; fi
 
 echo 'Travis Build Dir: '$TRAVIS_BUILD_DIR
 
-SOURCE_FOLDER=$TRAVIS_BUILD_DIR/testing/$target/chemkin/
+
+ORIGINAL=$TRAVIS_BUILD_DIR/testing/check/$target
+
+SOURCE_FOLDER=$TRAVIS_BUILD_DIR/testing/$target
 
 echo 'Source folder: '$SOURCE_FOLDER
 
 # check generated models:
 # core:
-python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_annotated.inp $SOURCE_FOLDER/species_dictionary.txt
+python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chemkin/chem_annotated.inp $SOURCE_FOLDER/chemkin/species_dictionary.txt
 grep "checkModels" $target.log > $target.core
 echo core for $target:
 cat $target.core
 
 # edge:
-python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_edge_annotated.inp $SOURCE_FOLDER/species_edge_dictionary.txt
+python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chemkin/chem_edge_annotated.inp $SOURCE_FOLDER/chemkin/species_edge_dictionary.txt
 grep "checkModels" $target.log > $target.edge
 echo edge for $target:
 cat $target.edge
+
+echo 'Execution time, Tested:'
+grep "Execution time" $TRAVIS_BUILD_DIR/testing/$target/RMG.log | tail -1
+echo 'Execution time, Original:'
+grep "Execution time" $ORIGINAL/RMG.log | tail -1
+
+echo 'Memory used, Tested:'
+grep "Memory used" $TRAVIS_BUILD_DIR/testing/$target/RMG.log | tail -1
+echo 'Memory used, Original:'
+grep "Memory used" $ORIGINAL/RMG.log | tail -1
