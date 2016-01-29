56e9e66d6689c20fdd8debfcb19fe6f7271a335f
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-28 21:45:27 -0500

    grep checkModels log file for "checkModels"
    and write to curated log file

--- check.sh
+++ check.sh
@@ -11,5 +11,8 @@ echo 'Source folder: '$SOURCE_FOLDER
 # check generated models:
 # core:
 python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_annotated.inp $SOURCE_FOLDER/species_dictionary.txt
+grep "checkModels" $target.log > $target.core
+
 # edge:
 python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_edge_annotated.inp $SOURCE_FOLDER/species_edge_dictionary.txt
+grep "checkModels" $target.log > $target.edge
