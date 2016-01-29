bd5420a24adf74d2a3d0b45bba4e52c8696da8db
Nick Vandewiele <nickvandewiele@gmail.com>
2016-01-28 22:10:30 -0500

    print curated log file to console

--- check.sh
+++ check.sh
@@ -12,7 +12,11 @@ echo 'Source folder: '$SOURCE_FOLDER
 # core:
 python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_annotated.inp $SOURCE_FOLDER/species_dictionary.txt
 grep "checkModels" $target.log > $target.core
+echo core for $target:
+cat $target.core
 
 # edge:
 python $TRAVIS_BUILD_DIR/checkModels.py $target $SOURCE_FOLDER/chem_edge_annotated.inp $SOURCE_FOLDER/species_edge_dictionary.txt
 grep "checkModels" $target.log > $target.edge
+echo edge for $target:
+cat $target.edge
