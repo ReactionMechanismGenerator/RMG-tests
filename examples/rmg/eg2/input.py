2afd87e0b9e57dafa89896413b01e954cee938b7
Nick Vandewiele <nickvandewiele@gmail.com>
2016-04-15 17:25:04 -0400

    add eg2 example (1,3-hexadiene, QMTP, MSC)

---
+++ examples/rmg/eg2/input.py
@@ -0,0 +1,92 @@
+# Data sources
+database(
+    thermoLibraries = ['primaryThermoLibrary', 'GRI-Mech3.0'],
+    reactionLibraries = [],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'], 
+    kineticsFamilies = 'default',
+    kineticsEstimator = 'rate rules',
+)
+
+# Constraints on generated species
+generatedSpeciesConstraints(
+    maximumRadicalElectrons = 2,
+)
+
+# List of species
+species(
+    label='HXD13',
+    reactive=True,
+    structure=SMILES("C=CC=CCC"),
+)
+species(
+    label='CH4',
+    reactive=True,
+    structure=SMILES("C"),
+)
+species(
+    label='H2',
+    reactive=True,
+    structure=adjacencyList(
+        """
+        1 H u0 p0 {2,S}
+        2 H u0 p0 {1,S}
+        """),
+)
+species(
+    label='N2',
+    reactive=False,
+    structure=InChI("InChI=1/N2/c1-2"),
+)
+
+# Reaction systems
+simpleReactor(
+    temperature=(1350,'K'),
+    pressure=(1.0,'bar'),
+    initialMoleFractions={
+        "HXD13": 6.829e-4,
+        "CH4": 0.104,
+        "H2": 0.0156,
+        "N2": 0.8797,
+    },
+    terminationConversion={
+        'HXD13': 0.9,
+    },
+    terminationTime=(1e0,'s'),
+)
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=0.5,
+    toleranceInterruptSimulation=0.5,
+    maximumEdgeSpecies=100000
+)
+
+quantumMechanics(
+    software='mopac',
+    method='pm3',
+    # fileStore='QMfiles', # relative to where it is run. Defaults within the output folder.
+    scratchDirectory = None, # not currently used
+    onlyCyclics = True,
+    maxRadicalNumber = 0,
+    )
+
+pressureDependence(
+    method='modified strong collision',
+    maximumGrainSize=(0.5,'kcal/mol'),
+    minimumNumberOfGrains=250,
+    temperatures=(300,2000,'K',8),
+    pressures=(0.01,100,'bar',5),
+    interpolation=('Chebyshev', 6, 4),
+)
+
+options(
+    units='si',
+    generateOutputHTML=False,
+    generatePlots=False,
+)
