3657829ede6f1fd24f8e900ac813c20050bc7569
Nick Vandewiele <nickvandewiele@gmail.com>
2016-06-29 15:53:03 -0400

    Revert "do not update anaconda"
    This reverts commit f16cfe91758c8306cbab54296cf7f2c3917462b3.

--- before_install.sh
+++ before_install.sh
@@ -5,3 +5,6 @@ wget http://repo.continuum.io/miniconda/Miniconda2-4.0.5-Linux-x86_64.sh -O mini
 chmod +x miniconda.sh
 ./miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
+
+# Update conda itself
+conda update --yes conda
