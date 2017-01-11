98032062ce6e6ced811cff63acb61608263ba30a
KEHANG <kehanghan@gmail.com>
2017-01-11 16:33:50 -0500

    prettify the version summary printout

--- version_summary.sh
+++ version_summary.sh
@@ -5,20 +5,21 @@ echo "========================================="
 echo "benchmark version of RMG: "$RMG_BENCHMARK
 cd $RMG_BENCHMARK
 git log --format=%H%n%cd -1
-cd -
+echo ""
+
+echo "testing version of RMG: "$RMG_TESTING  
+cd $RMG_TESTING
+git log --format=%H%n%cd -1
+echo ""
 
 echo "benchmark version of RMG-database: "$RMGDB_BENCHMARK
 cd $RMGDB_BENCHMARK
 git log --format=%H%n%cd -1
-cd -
-
-echo "test version of RMG: "$RMG_TESTING  
-cd $RMG_TESTING
-git log --format=%H%n%cd -1
-cd -
+echo ""
 
 echo "testing version of RMG-database: "$RMGDB_TESTING
 cd $RMGDB_TESTING
 git log --format=%H%n%cd -1
-cd -
 echo "========================================="
+
+cd $TRAVIS_BUILD_DIR
