7b618e64a4de7269027f9064bd57b5e450aabeb5
KEHANG <kehanghan@gmail.com>
2017-01-28 18:08:19 -0500

    seperate color definition from conda installation
    by doing so, local-tests can use the same color definition shell script

--- before_install.sh
+++ before_install.sh
@@ -8,9 +8,3 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 # Update conda itself
 conda update --yes conda
-
-# Define log colors
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export Yellow='\033[1;33m'
-export NC='\033[0m' # No Color
