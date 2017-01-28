267e3c055b0689fe8e7a6e8fdcd04532be69c91c
KEHANG <kehanghan@gmail.com>
2017-01-28 18:57:50 -0500

    use same variables between travis build mode and local mode
    so that the major body of install.sh can be used by both modes

--- before_install.sh
+++ before_install.sh
@@ -8,3 +8,37 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 # Update conda itself
 conda update --yes conda
+
+# Standardize dirs
+export BASE_DIR=$TRAVIS_BUILD_DIR
+export CURRENT_OS="linux"
+
+# Parse message for travis build
+# commit message of current head of RMG-tests = SHA1-ID of RMG-Py/database commit to be tested.
+MESSAGE=$(git log --format=%B -n 1 HEAD)
+echo "Message: "$MESSAGE
+
+export BRANCH=$TRAVIS_BRANCH
+echo "Branch: "$BRANCH
+
+# split the message on the '-' delimiter
+IFS='-' read -a msg_pieces <<< "$MESSAGE"
+IFS='-' read -a branch_pieces <<< "$BRANCH"
+
+if [ "${branch_pieces[0]}" == "rmgdb" ]; then
+	export RMG_TESTING_BRANCH="master"
+	export RMGDB_TESTING_BRANCH="${branch_pieces[1]}"
+
+elif [ "${branch_pieces[0]}" == "rmgpy" ]; then
+	export RMG_TESTING_BRANCH="${branch_pieces[1]}"
+	export RMGDB_TESTING_BRANCH="master"
+
+elif [ "${branch_pieces[0]}" == "rmgpydb" ]; then
+	export RMG_TESTING_BRANCH="${branch_pieces[1]}"
+	export RMGDB_TESTING_BRANCH="${msg_pieces[2]}"
+fi
+
+echo "RMG_TESTING_BRANCH: "$RMG_TESTING_BRANCH
+echo "RMGDB_TESTING_BRANCH: "$RMGDB_TESTING_BRANCH
+
+
--- install.sh
+++ install.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-# commit message of current head of RMG-tests = SHA1-ID of RMG-Py/database commit to be tested.
-MESSAGE=$(git log --format=%B -n 1 HEAD)
-echo "Message: "$MESSAGE
-
-export BRANCH=$TRAVIS_BRANCH
-echo "Branch: "$BRANCH
-
 # create a folder with benchmark version of RMG-Py and RMG-database:
 # go to parent-folder of the RMG-tests repo:
 cd ..
 # prepare benchmark RMG-Py and RMG-db
-export benchmark=$PWD/code/benchmark
+export benchmark=$BASE_DIR/code/benchmark
 rm -rf $benchmark
 mkdir -p $benchmark
 cd $benchmark
@@ -23,17 +16,11 @@ git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
 
 # check out the SHA-ID of the RMG-Py commit:
 cd RMG-Py
-sed -i -e 's/rmg_env/benchmark/g' environment_linux.yml
-conda env create -f environment_linux.yml # name will set by the name key in the environment yaml.
-git checkout environment_linux.yml
+sed -i -e 's/rmg_env/benchmark/g' environment_${CURRENT_OS}.yml
+conda env create -f environment_${CURRENT_OS}.yml # name will set by the name key in the environment yaml.
+git checkout environment_${CURRENT_OS}.yml
 
 export RMG_BENCHMARK=`pwd`
-
-# to show benchmark MG-Py HEAD
-echo ""
-echo "benchmark version of RMG: "$RMG_BENCHMARK
-git log --format=%H%n%cd -1
-echo ""
 
 # compile RMG-Py:
 source activate benchmark
@@ -47,84 +34,39 @@ git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
 cd RMG-database
 export RMGDB_BENCHMARK=`pwd`
 
-# to show benchmark RMG-database HEAD
-echo ""
-echo "benchmark version of RMG-database: "$RMGDB_BENCHMARK
-git log --format=%H%n%cd -1
-echo ""
 cd ..
 
 # prepare testing RMG-Py and RMG-db
-cd ..
-export testing=$PWD/code/testing
+export testing=$BASE_DIR/code/testing
 rm -rf $testing
 mkdir -p $testing
 cd $testing
 
-# split the message on the '-' delimiter
-IFS='-' read -a pieces <<< "$MESSAGE"
-
-# check if the first part of the splitted string is the "rmgdb" string:
-if [ "${pieces[0]}" == "rmgdb" ]; then
-  # pushed commit is of RMG-database
-  # message is of form: "rmgdb-SHA1"
-
-  # pushed commit is of RMG-database:
-  SHA1=${pieces[1]}
-  echo "SHA1: "$SHA1
-
+if [ $RMG_TESTING_BRANCH == "master" ]; then
   # set the RMG environment variable:
   export RMG_TESTING=$RMG_BENCHMARK
 
-  # to show testing RMG-Py HEAD
-  echo ""
-  echo "testing version of RMG: "$RMG_TESTING
-  cd $RMG_TESTING
-  git log --format=%H%n%cd -1
-  cd -
-  echo ""
   conda create --name testing --clone benchmark
 
   # check out the SHA-ID of the RMG-database commit:
   git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
   cd RMG-database
-  git checkout $SHA1
+  git checkout -b ${RMGDB_TESTING_BRANCH} origin/${RMGDB_TESTING_BRANCH}
   export RMGDB_TESTING=`pwd`
-
-  # to show testing RMG-database HEAD
-  echo ""
-  echo "testing version of RMG-database: "$RMGDB_TESTING
-  cd $RMG_TESTING
-  git log --format=%H%n%cd -1
-  cd -
-  echo ""
-  # return to parent directory:
   cd ..
 
-elif [ "${pieces[0]}" == "rmgpy" ]; then
-  # message is of form: "SHA1"
-
-  # pushed commit is of RMG-Py:
-  SHA1=${pieces[1]}
-  echo "SHA1: "$SHA1
-
+elif [ $RMGDB_TESTING_BRANCH == "master" ]; then
   # clone entire RMG-Py:
   git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
 
   # check out the SHA-ID of the RMG-Py commit:
   cd RMG-Py
-  git checkout $SHA1
-  sed -i -e 's/rmg_env/testing/g' environment_linux.yml
-  conda env create -f environment_linux.yml # name will set by the name key in the environment yaml.
+  git checkout -b ${RMG_TESTING_BRANCH} origin/${RMG_TESTING_BRANCH}
+  sed -i -e 's/rmg_env/testing/g' environment_${CURRENT_OS}.yml
+  conda env create -f environment_${CURRENT_OS}.yml
 
-  git checkout environment_linux.yml
-
-  # set the RMG environment variable:
-  echo ""
+  git checkout environment_${CURRENT_OS}.yml
   export RMG_TESTING=`pwd`
-  echo "testing version of RMG: "$RMG_TESTING
-  git log --format=%H%n%cd -1
-  echo ""
 
   # compile RMG-Py:
   source activate testing
@@ -133,55 +75,28 @@ elif [ "${pieces[0]}" == "rmgpy" ]; then
 
   # return to parent directory:
   cd ..
-
-  echo ""
   export RMGDB_TESTING=$RMGDB_BENCHMARK
-  echo "testing version of RMG-database: "$RMGDB_TESTING
-  cd $RMGDB_TESTING
-  git log --format=%H%n%cd -1
-  cd -
-  echo ""
 
-elif [ "${pieces[0]}" == "rmgpydb" ]; then
-  # message is of form: "SHA1"
-
-  # pushed commit is of RMG-Py:
-  SHA1=${pieces[1]}
-  echo "SHA1: "$SHA1
-
-  DB_branch=${pieces[2]}
-  echo "DB_branch: "${DB_branch}
-
+else
   # clone entire RMG-Py:
   git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
 
   # check out the SHA-ID of the RMG-Py commit:
   cd RMG-Py
-  git checkout $SHA1
-  sed -i -e 's/rmg_env/testing/g' environment_linux.yml
-  conda env create -f environment_linux.yml # name will set by the name key in the environment yaml.
+  git checkout -b ${RMG_TESTING_BRANCH} origin/${RMG_TESTING_BRANCH}
+  sed -i -e 's/rmg_env/testing/g' environment_${CURRENT_OS}.yml
+  conda env create -f environment_${CURRENT_OS}.yml
 
-  git checkout environment_linux.yml
+  git checkout environment_${CURRENT_OS}.yml
   export RMG_TESTING=`pwd`
 
-  # Show the RMG testing version:
-  echo ""
-  echo "testing version of RMG: "$RMG_TESTING
-  git log --format=%H%n%cd -1
-  echo ""
 
   # check out right RMG-database
   cd ..
   git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
   cd RMG-database
-  git checkout -b ${DB_branch} origin/${DB_branch}
+  git checkout -b ${RMGDB_TESTING_BRANCH} origin/${RMGDB_TESTING_BRANCH}
   export RMGDB_TESTING=`pwd`
-  
-  # Show the RMG database testing version:
-  echo ""
-  echo "testing version of RMG database: "$RMGDB_TESTING
-  git log --format=%H%n%cd -1
-  echo ""
 
   # go to RMG-Py
   cd ../RMG-Py
@@ -194,4 +109,4 @@ elif [ "${pieces[0]}" == "rmgpydb" ]; then
 fi
 
 # go to RMG-tests folder:
-cd $TRAVIS_BUILD_DIR
+cd $BASE_DIR
