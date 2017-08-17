2c7455811fc89a4b79888f3c781dbce9f1f0dd0c
KEHANG <kehanghan@gmail.com>
2017-08-17 13:39:49 -0400

    start data preparation module
    add methods of read dataset file and get data from
    database along with necessary tests

---
+++ thermo_val/data.py
@@ -0,0 +1,32 @@
+
+from pymongo import MongoClient
+
+def get_data(db_name, collection_name):
+
+    # connect to db and query
+    client = MongoClient('mongodb://user:user@rmg.mit.edu/admin', 27018)
+    db =  getattr(client, db_name)
+    collection = getattr(db, collection_name)
+    db_cursor = collection.find()
+
+    # collect data
+    print('Reading data from {0}:{1}...'.format(db_name, collection_name))
+    db_mols = []
+    for db_mol in db_cursor:
+        db_mols.append(db_mol)
+    print('Finish reading.')
+
+    return db_mols
+
+def get_datasets(dataset_file):
+
+    test_tables = []
+    with open(dataset_file, 'r') as f_in:
+        for line in f_in:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                host, db, table = [token.strip() for token in line.split('.')]
+
+                test_tables.append((host, db, table))
+
+    return test_tables
