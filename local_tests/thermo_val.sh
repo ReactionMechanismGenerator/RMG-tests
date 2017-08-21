a08711738fbab6c8716cb40444d4fce2d4f83bac
KEHANG <kehanghan@gmail.com>
2017-08-21 16:54:57 -0400

    update thermo_val bash script for test_mode

--- local_tests/thermo_val.sh
+++ local_tests/thermo_val.sh
@@ -8,9 +8,9 @@ source activate ${benchmark_env}
 echo "benchmark version of RMG: "$RMG_BENCHMARK
 export PYTHONPATH=$RMG_BENCHMARK:$ORIGIN_PYTHONPATH 
 
-python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hc_cyclics/dataset.txt
+python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hc_cyclics/dataset.txt -m benchmark
 
-python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hco_cyclics/dataset.txt
+python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hco_cyclics/dataset.txt -m benchmark
 
 source deactivate
 export PYTHONPATH=$ORIGIN_PYTHONPATH
@@ -22,9 +22,9 @@ source activate ${testing_env}
 echo "testing version of RMG: "$RMG_TESTING
 export PYTHONPATH=$RMG_TESTING:$ORIGIN_PYTHONPATH 
 
-python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hc_cyclics/dataset.txt
+python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hc_cyclics/dataset.txt -m testing
 
-python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hco_cyclics/dataset.txt
+python $BASE_DIR/thermo_val/evaluate.py -d $BASE_DIR/examples/thermo_val/hco_cyclics/dataset.txt -m testing
 
 source deactivate
 export PYTHONPATH=$ORIGIN_PYTHONPAT
