79e3f3c964be4c2021245a143050a9abe145f258
Max Liu <mjliu@mit.edu>
2018-06-29 18:15:32 -0400

    Use GitHub API token to push to RMG-tests
    Previously used nickv's ssh keys
    This way is also secure but much easier to maintain
    Remove deploy_key.enc since it's no longer needed

--- after_failure.sh
+++ after_failure.sh
@@ -11,21 +11,13 @@ echo "Executing after script tasks..."
     target_url=$BUILD_URL \
     | ./ok.sh _post $GITHUB_STATUS_PATH > /dev/null
 
-openssl aes-256-cbc -K $encrypted_95b1a418b9d1_key -iv $encrypted_95b1a418b9d1_iv -in deploy_key.enc -out deploy_key -d
-
 GIT_NAME="Travis Deploy"
 GIT_EMAIL="travisci@rmg.edu"
 echo GIT_NAME: $GIT_NAME
 echo GIT_EMAIL: $GIT_EMAIL
 
-# use the decrypted deploy SSH key as
-# the credentials to push to the RMG-tests repo:
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 
-REPO=git@github.com:ReactionMechanismGenerator/RMG-tests.git
+REPO=https://${GH_TOKEN}@github.com/ReactionMechanismGenerator/RMG-tests.git
 git push $REPO --delete $BRANCH > /dev/null
