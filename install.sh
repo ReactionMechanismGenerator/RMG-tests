a672b5bb19ddb21ee100c79d4a031bd07677605b
Max Liu <mjliu@mit.edu>
2018-07-02 11:40:14 -0400

    Refactor Travis build setup and scripts
    Use Travis build stages to increase available computing time
    Adjust build scripts to take into account build caching
    Simplify build scripts where possible

--- install.sh
+++ install.sh
@@ -1,114 +1,89 @@
 #!/bin/bash
 
-set -e
-
 # create a folder with benchmark version of RMG-Py and RMG-database:
 # go to parent-folder of the RMG-tests repo:
 cd ..
 # prepare benchmark RMG-Py and RMG-db
 export benchmark=$BASE_DIR/code/benchmark
-rm -rf $benchmark
 mkdir -p $benchmark
 cd $benchmark
 
 # clone benchmark RMG-Py
-git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+if [ ! -d "RMG-Py" ]; then
+  # the directory was not cached, so clone it
+  git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+  cd RMG-Py
+else
+  # the directory was cached, make sure that it's up to date
+  cd RMG-Py
+  git pull origin master
+fi
 
-# check out the SHA-ID of the RMG-Py commit:
-cd RMG-Py
-sed -i -e 's/rmg_env/benchmark/g' environment_${CURRENT_OS}.yml
-conda env create -q -f environment_${CURRENT_OS}.yml # name will set by the name key in the environment yaml.
-git checkout environment_${CURRENT_OS}.yml
+conda env create -n benchmark -f environment_${CURRENT_OS}.yml
 
 export RMG_BENCHMARK=`pwd`
-
-# compile RMG-Py:
-source activate benchmark
-make
-source deactivate
-
 cd ..
 
 # clone benchmark RMG-database:
-git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
-cd RMG-database
-export RMGDB_BENCHMARK=`pwd`
+if [ ! -d "RMG-database" ]; then
+  git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
+  cd RMG-database
+else
+  cd RMG-database
+  git pull origin master
+fi
 
+export RMGDB_BENCHMARK=`pwd`
 cd ..
 
 # prepare testing RMG-Py and RMG-db
 export testing=$BASE_DIR/code/testing
-rm -rf $testing
 mkdir -p $testing
 cd $testing
 
 if [ $RMG_TESTING_BRANCH == "master" ]; then
-  # set the RMG environment variable:
+  # set the RMG directory variable
   export RMG_TESTING=$RMG_BENCHMARK
-
+  # copy the conda environment
   conda create --name testing --clone benchmark
 
-  # check out the SHA-ID of the RMG-database commit:
-  git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
-  cd RMG-database
-  if [ $RMGDB_TESTING_BRANCH != "master" ]; then
-    git checkout -b ${RMGDB_TESTING_BRANCH} origin/${RMGDB_TESTING_BRANCH}
+else
+  # clone entire RMG-Py
+  if [ ! -d "RMG-Py" ]; then
+    git clone -b ${RMG_TESTING_BRANCH} https://github.com/ReactionMechanismGenerator/RMG-Py.git
+    cd RMG-Py
+  else
+    cd RMG-Py
+    git fetch origin ${RMG_TESTING_BRANCH}
+    git checkout ${RMG_TESTING_BRANCH}
+    git reset --hard origin/${RMG_TESTING_BRANCH}
   fi
-  export RMGDB_TESTING=`pwd`
-  cd ..
 
-elif [ $RMGDB_TESTING_BRANCH == "master" ]; then
-  # clone entire RMG-Py:
-  git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+  conda env create -n testing -f environment_${CURRENT_OS}.yml
 
-  # check out the SHA-ID of the RMG-Py commit:
-  cd RMG-Py
-  git checkout -b ${RMG_TESTING_BRANCH} origin/${RMG_TESTING_BRANCH}
-  sed -i -e 's/rmg_env/testing/g' environment_${CURRENT_OS}.yml
-  conda env create -q -f environment_${CURRENT_OS}.yml
-
-  git checkout environment_${CURRENT_OS}.yml
   export RMG_TESTING=`pwd`
-
-  # compile RMG-Py:
-  source activate testing
-  make
-  source deactivate
-
-  # return to parent directory:
   cd ..
+fi
+
+if [ $RMGDB_TESTING_BRANCH == "master" ]; then
+  # set the RMG database directory
   export RMGDB_TESTING=$RMGDB_BENCHMARK
 
 else
-  # clone entire RMG-Py:
-  git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+  # clone RMG-database
+  if [ ! -d "RMG-database" ]; then
+    git clone -b ${RMGDB_TESTING_BRANCH} https://github.com/ReactionMechanismGenerator/RMG-database.git
+    cd RMG-database
+  else
+    cd RMG-database
+    git fetch origin ${RMGDB_TESTING_BRANCH}
+    git checkout ${RMGDB_TESTING_BRANCH}
+    git reset --hard origin/${RMGDB_TESTING_BRANCH}
+  fi
 
-  # check out the SHA-ID of the RMG-Py commit:
-  cd RMG-Py
-  git checkout -b ${RMG_TESTING_BRANCH} origin/${RMG_TESTING_BRANCH}
-  sed -i -e 's/rmg_env/testing/g' environment_${CURRENT_OS}.yml
-  conda env create -q -f environment_${CURRENT_OS}.yml
-
-  git checkout environment_${CURRENT_OS}.yml
-  export RMG_TESTING=`pwd`
-
-
-  # check out right RMG-database
-  cd ..
-  git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
-  cd RMG-database
-  git checkout -b ${RMGDB_TESTING_BRANCH} origin/${RMGDB_TESTING_BRANCH}
   export RMGDB_TESTING=`pwd`
-
-  # go to RMG-Py
-  cd ../RMG-Py
-
-  # compile RMG-Py:
-  source activate testing
-  make
-  source deactivate
-
+  cd ..
 fi
 
-# go to RMG-tests folder:
+# go to RMG-tests folder
 cd $BASE_DIR
