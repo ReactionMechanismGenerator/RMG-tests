c11cc4a564ce736c61222ac9a7f18eb356afe7fd
Sevy Harris <harris.se@northeastern.edu>
2021-10-18 13:15:55 -0400

    downgrade StochasticDiffEq version for compatibility

--- install.sh
+++ install.sh
@@ -107,16 +107,14 @@ echo "BENCHMARK_CONDA_ENV=$BENCHMARK_CONDA_ENV" >> $GITHUB_ENV
 # Install and link Julia dependencies
 echo "Installing and linking Julia dependencies"
 conda activate $BENCHMARK_CONDA_ENV
-julia -e "using Pkg; Pkg.add(PackageSpec(url=\"https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl\", rev=\"main\"))"
-julia -e "using Pkg; Pkg.add(\"PyCall\"); Pkg.add(\"DifferentialEquations\")"
-python-jl -c "import julia; julia.install()"
+python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
+julia -e 'using Pkg; Pkg.add(PackageSpec(name="StochasticDiffEq",version="6.36.0")); Pkg.add(PackageSpec(name="ReactionMechanismSimulator",version="0.4")); using ReactionMechanismSimulator;'
 ln -sfn $(which python-jl) $(which python)
 conda deactivate
 
 conda activate $TESTING_CONDA_ENV
-julia -e "using Pkg; Pkg.add(PackageSpec(url=\"https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl\", rev=\"main\"))"
-julia -e "using Pkg; Pkg.add(\"PyCall\"); Pkg.add(\"DifferentialEquations\")"
-python-jl -c "import julia; julia.install()"
+python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
+julia -e 'using Pkg; Pkg.add(PackageSpec(name="StochasticDiffEq",version="6.36.0")); Pkg.add(PackageSpec(name="ReactionMechanismSimulator",version="0.4")); using ReactionMechanismSimulator;'
 ln -sfn $(which python-jl) $(which python)
 conda deactivate
 
