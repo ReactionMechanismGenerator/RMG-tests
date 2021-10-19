a5c1d61083ad65b22d05f66b53b316d377607a23
Sevy Harris <harris.se@northeastern.edu>
2021-10-19 16:27:24 -0400

    Put quotations around the python path
    The path in the Julia command needs quotes areound it or Julia complains

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ echo "BENCHMARK_CONDA_ENV=$BENCHMARK_CONDA_ENV" >> $GITHUB_ENV
 echo "Installing and linking Julia dependencies"
 conda activate $BENCHMARK_CONDA_ENV
 PYTHONPATH=$RMG_BENCHMARK:$PYTHONPATH
-julia -e 'ENV["PYTHON"]='$BENCHMARK_CONDA_ENV'/bin/python; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall")'
+julia -e 'ENV["PYTHON"]="'$BENCHMARK_CONDA_ENV'/bin/python"; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall")'
 python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
 julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",rev="main")); Pkg.add(PackageSpec(name="StochasticDiffEq",version="6.36.0")); using ReactionMechanismSimulator'
 ln -sfn $(which python-jl) $(which python)
@@ -116,7 +116,7 @@ conda deactivate
 
 conda activate $TESTING_CONDA_ENV
 PYTHONPATH=$RMG_TESTING:$PYTHONPATH
-julia -e 'ENV["PYTHON"]='$TESTING_CONDA_ENV'/bin/python; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall")'
+julia -e 'ENV["PYTHON"]="'$TESTING_CONDA_ENV'/bin/python"; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall")'
 python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
 julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",rev="main")); Pkg.add(PackageSpec(name="StochasticDiffEq",version="6.36.0")); using ReactionMechanismSimulator'
 ln -sfn $(which python-jl) $(which python)
